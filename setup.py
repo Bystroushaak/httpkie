393c89494fd1677822acc3fd0d683ac254cac856
Bystroushaak <bystrousak@kitakitsune.org>
2014-03-22 15:30:44 +0100

    PEP8 aplied. Static files moved from setup.py into MANIFEST.in.

--- setup.py
+++ setup.py
@@ -1,39 +1,32 @@
 #!/usr/bin/env python
-import os
 from distutils.core import setup
 
 
-def dirToLinks(directory):
-	return map(lambda x: directory + "/" + x, os.listdir(directory))
-
-
 url = 'https://github.com/Bystroushaak/httpkie'
+long_description = """
+Documentation can be found in README.creole, or at project pages at github: 
+""" + url
 
 setup(
-	name         = 'httpkie',
-	version      = '1.0.0',
-	py_modules   = ['httpkie'],
+    name         = 'httpkie',
+    version      = '1.1.0',
+    py_modules   = ['httpkie'],
 
-	author       = 'Bystroushaak',
-	author_email = 'bystrousak@kitakitsune.org',
+    author       = 'Bystroushaak',
+    author_email = 'bystrousak@kitakitsune.org',
 
-	url          = url,
-	description  = 'Easy to use web scrapper with cookies and proxy support.',
-	license      = 'CC BY',
+    url          = url,
+    description  = 'Easy to use web scrapper with cookies and proxy support.',
+    license      = 'CC BY',
 
-	long_description = """Documentation can be found in README.creole, or at
-project pages at github: """ + url,
+    long_description=long_description,
 
-	classifiers=[
-		"License :: Public Domain",
+    classifiers=[
+        "License :: Public Domain",
 
-		"Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 2.7",
 
-		"Topic :: Software Development :: Libraries",
-		"Topic :: Internet :: WWW/HTTP"
-	],
-
-	data_files=[
-		('others', ['README.creole', "__init__.py"]),
-	]
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Internet :: WWW/HTTP"
+    ],
 )
